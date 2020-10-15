0b59bf7503a53e101709828a580657516dd82c73
Ualzem <arquivosualzem@gmail.com>
2020-10-15 13:31:34 -0300

    commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/Logo.svg';
+import { FiArrowRight } from 'react-icons/fi';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="page-landing">
+
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+<main>
+  <h1>Leve felicidade para o mundo</h1>
+  <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+</main>
+
+<div className="location">
+  <strong>Pitangui</strong>
+  <span>Minas Gerais</span>
+</div>
+
+<a href="" className="enter-app">
+ <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+</a>
+
+</div>
+</div>
   );
 }
 
